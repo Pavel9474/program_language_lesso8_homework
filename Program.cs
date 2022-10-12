39af4d56bdd5737f8ad1bd45b8189ec1c7501bd6
Pavel <pavel.sharagin.1994@gmail.com>
2022-10-12 21:00:53 +0500

    addtask2

--- Program.cs
+++ Program.cs
@@ -1,17 +1,11 @@
-﻿// Написать рекурсивный метод, проверяющий равенство
-//сумм цифр одного числа и второго числа
-int a=125;
-int b=8;
-sum(a);
-sum(b);
-if (sum(a)==sum(b))
-Console.WriteLine("Суммы цифр равны");
-else
-Console.WriteLine("Суммы цифр не равны");
-int sum (int n)
-{
-    if (n/10!=0)
-        return n%10+sum(n/10);
-    else
-        return n%10;
-}
+﻿// Написать рекурсивный метод, рассчитывающий
+//количество делителей числа
+int a=6;
+division(a);
+void division (int n, int i = 1)
+    {
+        if (n % i == 0) 
+            Console.Write(i + " ");
+        if (i == n) return;
+        division(n, ++i);
+    }
