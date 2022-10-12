87841ab882c454503b8d1efedb7fb106dd289fbe
Pavel <pavel.sharagin.1994@gmail.com>
2022-10-12 20:31:15 +0500

    addtask1done

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать рекурсивный метод, проверяющий равенство
+//сумм цифр одного числа и второго числа
+int a=125;
+int b=8;
+sum(a);
+sum(b);
+if (sum(a)==sum(b))
+Console.WriteLine("Суммы цифр равны");
+else
+Console.WriteLine("Суммы цифр не равны");
+int sum (int n)
+{
+    if (n/10!=0)
+        return n%10+sum(n/10);
+    else
+        return n%10;
+}
