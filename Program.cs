abf33973f64482435a7fa41f4d7165a62fe0e0c6
pavel.sharagin.1994@gmail.com <pavel.sharagin.1994@gmail.com>
2022-10-12 15:57:25 +0500

    task54 done

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetRandomValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = GetRandomValue(minValue,maxValue);
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine(); 
+}
+int[,] InitializateArray(int m, int n)
+{
+    return new int[m,n];
+}
+int [,] SortArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+  return array;
+}
+int m = GetNumberFromConsole("Введите размерность массива m");
+int n = GetNumberFromConsole("Введите размерность массива n");
+int[,] array = InitializateArray(m,n);
+int minValue = GetNumberFromConsole("Введите минимальное число, допустимое в массиве");
+int maxValue = GetNumberFromConsole("Введите максимальное число, допустимое в массиве");
+FillArray(array,minValue,maxValue);
+PrintArray(array);
+PrintArray(SortArrayLines(array));
